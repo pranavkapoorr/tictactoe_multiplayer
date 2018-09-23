d3f4a6508cf9b252f82c814aa087ca13ea79648e
Pranav Kapoor <pranavkapoorr@gmail.com>
2018-09-23 19:57:05 -0400

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:tictactoe/home_page.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: new ThemeData(primaryColor: Colors.black),
+      home: new HomePage(),
+    );
+  }
+}
